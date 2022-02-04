78bd0a6453fed786ff715ef570eec4f40f0a343b
Anita-Zhou <1011433458@qq.com>
2022-02-04 14:45:45 -0600

    change hitbox name

--- Demo2/GameScripts/Stats.gd
+++ Demo2/GameScripts/Stats.gd
@@ -1,17 +1,16 @@
 extends Node
 
 export(int) var max_health = 100
-var health = max_health
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var health = max_health setget set_health
 
+signal no_health
+signal health_changed(value)
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func set_health(value):
+	health = value
+	emit_signal("health_changed", health)
+	if health <= 0:
+		emit_signal("no_health")
