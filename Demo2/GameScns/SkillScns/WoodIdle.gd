103d9ae4e15f800380525f60cb84012b3f575b8e
Anita-Zhou <1011433458@qq.com>
2022-02-03 23:19:43 -0600

    waterskill being able to trigger wood skill

--- Demo2/GameScns/SkillScns/WoodIdle.gd
+++ Demo2/GameScns/SkillScns/WoodIdle.gd
@@ -10,12 +10,12 @@ extends Node2D
 onready var water = get_node("WaterSkill")
 func _on_Hurtbox_area_entered(area):
 	#area != boar and 
-	print(str(area.get_parent()))
-	if area == water:	# call wood skill
-		var WoodSkill = load("res://GameScns/SkillScns/WoodSkill.tscn")
-		var woodSkill = WoodSkill.instance()
-		var world = get_tree().current_scene
-		world.add_child(woodSkill)
-		woodSkill.global_position = global_position
-		queue_free()
+	#print(str(area.get_parent()))
+	#if area == water:	# call wood skill
+	var WoodSkill = load("res://GameScns/SkillScns/WoodSkill.tscn")
+	var woodSkill = WoodSkill.instance()
+	var world = get_tree().current_scene
+	world.add_child(woodSkill)
+	woodSkill.global_position = global_position
+	queue_free()
 	
