c5bc0a159758fc7a5bb3e0569c21ae56c0676baa
Anita-Zhou <1011433458@qq.com>
2022-02-18 00:45:22 -0600

    having bug, unable to unpause

--- Demo2/GameScripts/UIScripts/RestartBtn.gd
+++ Demo2/GameScripts/UIScripts/RestartBtn.gd
@@ -1,29 +1,19 @@
 extends Button
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _get_configuration_warning()->String:
+	return "cannot load World1 scene"
 
 
-func _on__button_up():
+func _on_RestartBtn_pressed():
+	print("restart button pressed")
 	PlayerStats.health = PlayerStats.max_health
 	get_tree().paused = false
 	print("get_tree().paused = false")
 	get_tree().reload_current_scene()
 	print("get_tree().reload_current_scene()")
-	
-func _get_configuration_warning()->String:
-	return "cannot load World1 scene"
-	
-	
+	get_tree().change_scene("res://World1.tscn")
+	print("get_tree().change_scene()")
