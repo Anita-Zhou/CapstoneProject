40aeebfadca053698bc21090b4fc03df19612235
Anita-Zhou <1011433458@qq.com>
2022-02-25 13:58:06 -0600

    main menu

--- Demo2/LevelScripts/LevelBtn.gd
+++ Demo2/LevelScripts/LevelBtn.gd
@@ -16,11 +16,6 @@ func _ready():
 #	pass
 
 
-func _on_MainTxtBtn_button_up():
-	print("=====pressed main menu button")
-	PlayerStats.reset()
-	get_tree().change_scene("res://Levels/SplashScreen.tscn")
+func _on_LevelBtn_button_up():
+	get_tree().change_scene("res://Levels/LevelSelect.tscn")
 
-
-func _on_button_up():
-	pass # Replace with function body.
--- Demo2/LevelScripts/PlayBtn.gd
+++ Demo2/LevelScripts/PlayBtn.gd
@@ -16,11 +16,5 @@ func _ready():
 #	pass
 
 
-func _on_MainTxtBtn_button_up():
-	print("=====pressed main menu button")
-	PlayerStats.reset()
-	get_tree().change_scene("res://Levels/SplashScreen.tscn")
-
-
-func _on_button_up():
-	pass # Replace with function body.
+func _on_PlayBtn_button_up():
+	get_tree().change_scene("res://Levels/World1.tscn")
