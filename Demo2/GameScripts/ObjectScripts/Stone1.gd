49fe77d393ff5275215be8366ea4f02ea26deff9
Cuiyuanxiu Chen <cuiyuanxiuchen@cuiyuanxius-mbp.dyndns.rice.edu>
2022-02-24 20:57:39 -0600

    tutorial art

--- Demo2/GameScripts/ObjectScripts/Stone1.gd
+++ Demo2/GameScripts/ObjectScripts/Stone1.gd
@@ -6,8 +6,8 @@ func create_stone_effect():
 	var world = get_tree().current_scene
 	world.add_child(stoneEffect)
 	stoneEffect.global_position = global_position
-
-func _on_Hurtbox_area_entered(area):
-	create_stone_effect()
-	queue_free()
+#
+#func _on_Hurtbox_area_entered(area):
+#	create_stone_effect()
+#	queue_free()
 	
