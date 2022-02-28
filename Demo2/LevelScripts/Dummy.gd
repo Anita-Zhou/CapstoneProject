0619c15bda9c8e10b7672248737a0cc155fcf326
Anita-Zhou <1011433458@qq.com>
2022-02-28 14:40:55 -0600

    dummmy moving

--- Demo2/LevelScripts/Dummy.gd
+++ Demo2/LevelScripts/Dummy.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends KinematicBody2D
 
 onready var screenSize = get_viewport().get_visible_rect().size
 onready var animationPlayer = $AnimationPlayer
@@ -6,7 +6,29 @@ onready var stats = $Stats
 
 var direction = Vector2(0, 0)
 var temp_direction = Vector2(0, 0)
-var speed = 40
+#var direction = Vector2(0, 0)
+var distance2hero = Vector2(0, 0)
+var speed = 15
+var timer = 0
+
+onready var player = null
+
+func _ready():
+	print("=====dummy ready")
+#	rng.randomize()
+	player = get_parent().get_node("Player")
+	print("=====dummy got player")
+	
+func _physics_process(delta):
+	var motion = direction * speed
+	if(is_instance_valid(player)):
+		direction = player.position - self.position
+		distance2hero = self.position.distance_to(player.position)
+	direction = direction.normalized()
+	#print("current dummy direction:", direction)
+
+	move_and_slide(motion)
+	move_and_collide(motion * delta)
 
 #func _physics_process(delta):
 #	var motion = direction * speed
