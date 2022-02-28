9537ae4cb907f7814c45cee8f47ea3158ed26ac4
Cuiyuanxiu Chen <cuiyuanxiuchen@cuiyuanxius-mbp.dyndns.rice.edu>
2022-02-28 14:06:24 -0600

    game cycle

--- Demo2/LevelScripts/Dummy.gd
+++ Demo2/LevelScripts/Dummy.gd
@@ -1,15 +1,28 @@
 extends Node2D
-
+#
+#onready var screenSize = get_viewport().get_visible_rect().size
 onready var animationPlayer = $AnimationPlayer
+#onready var animationTree = $AnimationTree
+#onready var animationState = animationTree.get("parameters/playback")
 onready var stats = $Stats
+#
+#var direction = Vector2(0, 0)
+#var temp_direction = Vector2(0, 0)
+#var speed = 40
+#
+#enum{
+#	JUMP
+#}
+#var state = JUMP
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
+#func _physics_process(delta):
+#	var motion = direction * speed
+#	match state:
+#		JUMP:
+#			motion = direction * speed
+#			animationState.travel("Jump")
+#
 func _on_Hurtbox_area_entered(area):
 	take_damage(area)
 	
