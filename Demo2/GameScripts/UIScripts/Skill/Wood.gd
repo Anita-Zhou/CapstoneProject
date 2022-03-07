9deeb823354971dc809855abb6efdbbe14547aab
Anita-Zhou <1011433458@qq.com>
2022-03-06 23:36:05 -0600

    cooldown success

--- Demo2/GameScripts/UIScripts/Skill/Wood.gd
+++ Demo2/GameScripts/UIScripts/Skill/Wood.gd
@@ -2,36 +2,37 @@ extends Sprite
 
 var block_sig = ""
 var unblock_sig = ""
-var skill_name = self.get_name()
 var cd_unit = 0.0
 var full_cd = 0.0
 
 onready var cd = $SkillCD
+onready var skill_name = self.get_name()
 onready var stats = $"/root/PlayerStats"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-#	print("skill name:", skill_name)
-#	match skill_name:
-#		"Wood":
-#			block_sig = "wood_block"
-#			unblock_sig = "wood_unblock"
-#			full_cd = 300.0
-#		"Water":
-#			block_sig = "water_block"
-#			unblock_sig = "water_unblock"
-#			full_cd = 420.0
-#	PlayerStats.connect(block_sig, self, "_draw")
-#	PlayerStats.connect(unblock_sig, self, "_erase")
+#	skill_name = self.get_name()
+	print("skill name:", skill_name)
+	match skill_name:
+		"Wood":
+			print("skill is wood")
+			block_sig = "wood_block"
+			unblock_sig = "wood_unblock"
+			full_cd = 300.0
+		"Water":
+			block_sig = "water_block"
+			unblock_sig = "water_unblock"
+			full_cd = 420.0
+	PlayerStats.connect(block_sig, self, "_draw")
+	PlayerStats.connect(unblock_sig, self, "_erase")
 	
 func _physics_process(delta):
-#	cd_unit = stats._get_cd(skill_name) / 100.0
-#	cd.value = stats._get_cd(skill_name) / cd_unit
-	pass
+	cd_unit = full_cd / 100.0
+	cd.value = stats._get_cd(skill_name) / cd_unit
+#	pass
 	
-func _draw():
-	pass
-
-func _erase():
-	pass
+#func _draw():
+#	pass
+#
+#func _erase():
+#	pass
