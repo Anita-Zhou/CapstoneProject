8fe4c155d88bc039cb6190eb0c0258dceed62668
Anita-Zhou <1011433458@qq.com>
2022-03-06 23:30:44 -0600

    cooldown implementing:

--- Demo2/GameScripts/UIScripts/Skill/Wood.gd
+++ Demo2/GameScripts/UIScripts/Skill/Wood.gd
@@ -11,18 +11,19 @@ onready var stats = $"/root/PlayerStats"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("skill name:", skill_name)
-	match skill_name:
-		"Wood":
-			block_sig = "wood_block"
-			unblock_sig = "wood_unblock"
-			full_cd = 300.0
-		"Water":
-			block_sig = "water_block"
-			unblock_sig = "water_unblock"
-			full_cd = 420.0
-	PlayerStats.connect(block_sig, self, "_draw")
-	PlayerStats.connect(unblock_sig, self, "_erase")
+	pass
+#	print("skill name:", skill_name)
+#	match skill_name:
+#		"Wood":
+#			block_sig = "wood_block"
+#			unblock_sig = "wood_unblock"
+#			full_cd = 300.0
+#		"Water":
+#			block_sig = "water_block"
+#			unblock_sig = "water_unblock"
+#			full_cd = 420.0
+#	PlayerStats.connect(block_sig, self, "_draw")
+#	PlayerStats.connect(unblock_sig, self, "_erase")
 	
 func _physics_process(delta):
 #	cd_unit = stats._get_cd(skill_name) / 100.0
